7a072c60eb0a43d81fccf1f0a4ba63124022e393
jemtca <30645648+jemtca@users.noreply.github.com>
2017-12-12 17:26:33 -0800

    lab06 updates

--- Labs/Lab06/InputOutput.java
+++ Labs/Lab06/InputOutput.java
@@ -1,7 +1,47 @@
 package ca.ciccc.java.view;
 
-public class InputOutput {
+public class InputOutput extends Exception{
 	
+	public InputOutput() {
+		
+		
+		
+	}
 	
+	public String[] splitDate(String date) {
+		
+		String[] s;
+		
+		s = date.split("-");
+		
+		return s;
+	
+	}
+	
+	private boolean dateValidation(String date) {
+		
+		boolean result = false;
+		
+		//1111-11-11
+		try {
+			
+			int year = Integer.parseInt(date.substring(0,4));
+			int month = Integer.parseInt(date.substring(5,7));
+			int day = Integer.parseInt(date.substring(8));
+		
+		}
+		catch(NumberFormatException nfe) {
+			
+			nfe.getMessage();
+			
+		}
+		
+		char s4 = date.charAt(4);
+		char s5 = date.charAt(7);
+		
+		return result;
+		
+		
+	}
 
 }
--- Labs/Lab06/Main.java
+++ Labs/Lab06/Main.java
@@ -1,11 +1,27 @@
 package ca.ciccc.java.driver;
 
+import ca.ciccc.java.helper.InputReader;
+import ca.ciccc.java.view.InputOutput;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		InputReader ir = new InputReader();
+		InputOutput io = new InputOutput();
 		
-
+		String input;
+		
+		do {
+			
+			System.out.print("Type a date (yyyy-mm-dd) or \"quit\" to exit: ");
+			input = ir.getStringInput();
+			
+			System.out.println(input);
+			
+		}
+		while(input.compareToIgnoreCase("quit") != 0);
+		
 	}
 
 }
